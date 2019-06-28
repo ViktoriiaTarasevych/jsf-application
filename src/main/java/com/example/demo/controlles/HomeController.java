2010cb0f3800a08bc1793461072936b527f19494
viktoriiaw <viktoriia.t@ukr.net>
2019-06-28 20:32:43 +0300

    Добавила страницу "Личные данные"

--- src/main/java/com/example/demo/controlles/HomeController.java
+++ src/main/java/com/example/demo/controlles/HomeController.java
@@ -9,6 +9,26 @@ public class HomeController {
 
     private String surname;
 
+    private String yearBirthday;
+
+    private String numberPassport;
+
+    public String getNumberPassport() {
+        return numberPassport;
+    }
+
+    public void setNumberPassport(String numberPassport) {
+        this.numberPassport = numberPassport;
+    }
+
+    public String getYearBirthday() {
+        return yearBirthday;
+    }
+
+    public void setYearBirthday(String yearBirthday) {
+        this.yearBirthday = yearBirthday;
+    }
+
     public String getName() {
         return name;
     }
@@ -27,7 +47,7 @@ public class HomeController {
 
     public String sayHello1() {
         System.out.println("Hello2 " + name + " " + surname);
-        return "home.xhtml?faces-redirect=true&name=" + name;
+        return "hello.xhtml?faces-redirect=true&name=" + name;
     }
 
     public void sayHello2() {
