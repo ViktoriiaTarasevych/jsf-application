b102a9bccec661c9cff29db66c8e4012208f3063
viktoriiaw <viktoriia.t@ukr.net>
2019-06-29 23:16:21 +0300

    Переименовала методы

--- src/main/java/com/example/demo/controlles/HomeController.java
+++ src/main/java/com/example/demo/controlles/HomeController.java
@@ -15,20 +15,44 @@ public class HomeController {
     private String periodOfResidenceBegin;
     private String periodOfResidenceEnd;
 
-    public String getPeriodOfResidenceBegin() {
-        return periodOfResidenceBegin;
+    public String getName() {
+        return name;
     }
 
-    public void setPeriodOfResidenceBegin(String periodOfResidenceBegin) {
-        this.periodOfResidenceBegin = periodOfResidenceBegin;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public String getPeriodOfResidenceEnd() {
-        return periodOfResidenceEnd;
+    public String getSurname() {
+        return surname;
     }
 
-    public void setPeriodOfResidenceEnd(String periodOfResidenceEnd) {
-        this.periodOfResidenceEnd = periodOfResidenceEnd;
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getYearBirthday() {
+        return yearBirthday;
+    }
+
+    public void setYearBirthday(String yearBirthday) {
+        this.yearBirthday = yearBirthday;
+    }
+
+    public String getNumberPassport() {
+        return numberPassport;
+    }
+
+    public void setNumberPassport(String numberPassport) {
+        this.numberPassport = numberPassport;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
     }
 
     public String getCity() {
@@ -47,55 +71,35 @@ public class HomeController {
         this.countPeople = countPeople;
     }
 
-    public String getCountry() {
-        return country;
+    public String getPeriodOfResidenceBegin() {
+        return periodOfResidenceBegin;
     }
 
-    public void setCountry(String country) {
-        this.country = country;
+    public void setPeriodOfResidenceBegin(String periodOfResidenceBegin) {
+        this.periodOfResidenceBegin = periodOfResidenceBegin;
     }
 
-    public String getNumberPassport() {
-        return numberPassport;
+    public String getPeriodOfResidenceEnd() {
+        return periodOfResidenceEnd;
     }
 
-    public void setNumberPassport(String numberPassport) {
-        this.numberPassport = numberPassport;
+    public void setPeriodOfResidenceEnd(String periodOfResidenceEnd) {
+        this.periodOfResidenceEnd = periodOfResidenceEnd;
     }
 
-    public String getYearBirthday() {
-        return yearBirthday;
-    }
-
-    public void setYearBirthday(String yearBirthday) {
-        this.yearBirthday = yearBirthday;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String sayHello1() {
-        return "dataForHotel.xhtml?faces-redirect=true&name=";
-    }
-
-    public String sayHello3() {
+    public String moveToQuestionAboutPersonalData() {
         return "personalData.xhtml?faces-redirect=true&name=";
     }
 
-    public String sayHello4() {
+    public String moveToQuestionAboutDataForHotel() {
+        return "dataForHotel.xhtml?faces-redirect=true&name=";
+    }
+
+    public String moveToQuestionAboutControl() {
         return "control.xhtml?faces-redirect=true&name=";
+    }
+
+    public String moveToLastPage() {
+        return "lastPage.xhtml?faces-redirect=true&name=";
     }
 }
